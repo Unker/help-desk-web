e16247da60c91493e8eed4808c723dafd36f742d
unker <kfi.unknown@gmail.com>
2023-09-16 21:32:21 +0300

    del server port

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import TicketList from '../components/ticket-list/ticket-list';
 
 console.log('env server:', process.env.SERVER_URL);
-const ticketList = new TicketList(process.env.SERVER_URL || 'https://help-desk-server.onrender.com:7000');
+const ticketList = new TicketList(process.env.SERVER_URL || 'https://help-desk-server.onrender.com');
